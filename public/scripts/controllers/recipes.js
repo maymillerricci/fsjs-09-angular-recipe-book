5cfc8e4ed96ad8a5d1592750fd8b23e5e7ec4a13
May Miller-Ricci <maymillerricci@gmail.com>
2017-01-10 08:53:30 -0800

    filter recipe list by category

--- public/scripts/controllers/recipes.js
+++ public/scripts/controllers/recipes.js
@@ -4,9 +4,22 @@ angular.module('app')
 .controller('RecipesController', function($scope, dataService) {
   dataService.getCategories(function(response) {
     $scope.categories = response.data;
+    $scope.categories.unshift({ name: "All Categories" });
   });
 
   dataService.getRecipes(function(response) {
     $scope.recipes = response.data;
   });
+
+  $scope.getRecipesByCategory = function(category) {
+    if (category === "All Categories") {
+      dataService.getRecipes(function(response) {
+        $scope.recipes = response.data;
+      });
+    } else {
+      dataService.getRecipesByCategory(category, function(response) {
+        $scope.recipes = response.data;
+      });
+    }
+  }
 });
