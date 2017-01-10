9c8b20c5273d4fa1933cef16873dde44293f31f8
May Miller-Ricci <maymillerricci@gmail.com>
2017-01-10 00:00:23 -0800

    get recipes and categories in controller and use ng-repeat to display them on recipes page

--- public/scripts/controllers/recipes.js
+++ public/scripts/controllers/recipes.js
@@ -2,5 +2,11 @@
 
 angular.module('app')
 .controller('RecipesController', function($scope, dataService) {
+  dataService.getCategories(function(response) {
+    $scope.categories = response.data;
+  });
 
+  dataService.getRecipes(function(response) {
+    $scope.recipes = response.data;
+  });
 });
